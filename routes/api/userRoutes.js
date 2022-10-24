fc0dc39ee94a8aa0aead428898566b4aab965a7c
mlcundayag <mlcundayag@gmail.com>
2022-10-24 02:10:43 -1000

    pseudo code userRoute

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const {
+   getUsers,
+   getSingleUser,
+   createUser,
+   updateUser,
+   removeUser,
+   addFriend,
+   removeFriend
+} = require('../../controllers/userController')
+
+// /api/users/
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(removeUser);
+
+// /api/users/:userId/friends/:friendsId
+router.route('/:userId/friends/:friendsId').post(addFriend).delete(removeFriend)
+
+module.exports = router
