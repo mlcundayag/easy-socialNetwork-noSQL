d8f47f32b2f8db1eb90edda424883b04c83f2b22
mlcundayag <mlcundayag@gmail.com>
2022-10-25 02:16:23 -1000

    created get single thought and put route

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -39,6 +39,40 @@ router.post('/', async (req, res) => {
     }
 })
 
+router.get('/:thoughtId', async (req, res) => {
+    try{
+        const getSingleThought = await Thought.findOne({_id: req.params.thoughtId});
+        if(!getSingleThought){
+            return res.status(400).json({ message: "No thought with this Id... Try again!" })
+        } res.status(200).json(getSingleThought)
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).json(err)
+    }
+})
+
+router.put('/:thoughtId', async (req, res) => {
+    try{
+        const updateThought = await Thought.findOneAndUpdate({
+            _id: req.params.thoughtId
+        },
+        {
+           $set: req.body
+        }, 
+        {
+            new: true,
+            runValidators: true
+        });
+        if (!updateThought) {
+            return res.status(404).json({ message: "No thought with this Id... Try again!" })
+        } res.status(200).json(updateThought)
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).json(err)
+    }  
+})
 
 
 module.exports = router
