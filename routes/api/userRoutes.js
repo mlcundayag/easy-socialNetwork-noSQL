0b94a0395d804746df05b7e9708c4265a639d93f
mlcundayag <mlcundayag@gmail.com>
2022-10-25 00:48:01 -1000

    route put and delete by userId

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../../models')
+const { User, Thought } = require('../../models');
 
 // const {
 //    getUsers,
@@ -13,11 +13,11 @@ const { User } = require('../../models')
 
 //GET: /api/users
 router.get('/', async (req, res) => {
-    try{
+    try {
         const getAllUsers = await User.find({})
-        .select("-__v")
-        .populate("friends")
-        .populate("thoughts")
+            .select("-__v")
+            .populate("friends")
+            .populate("thoughts")
         res.status(200).json(getAllUsers)
     }
     catch (err) {
@@ -28,7 +28,7 @@ router.get('/', async (req, res) => {
 
 //POST: /api/users
 router.post('/', async (req, res) => {
-    try{
+    try {
         const createUser = await User.create(req.body);
         res.status(200).json(createUser)
     }
@@ -38,18 +38,58 @@ router.post('/', async (req, res) => {
     }
 })
 
-//GET: /api/users/userID
+//GET: /api/users/:userId
 router.get('/:userId', async (req, res) => {
-    try{
+    try {
         const getSingleUser = await User.findOne({ _id: req.params.userId })
-        .select("-__v")
-        .populate('friends')
-        .populate('thoughts')
-        if(!getSingleUser) {
-            return res.status(404).json({ message: "No user with this ID"})
+            .select("-__v")
+            .populate('friends')
+            .populate('thoughts')
+        if (!getSingleUser) {
+            return res.status(404).json({ message: "No user with this ID... Try again!" })
         } res.status(200).json(getSingleUser)
     }
     catch (err) {
+        console.log(err)
+        res.status(500).json(err)
+    }
+})
+
+//PUT: /api/users/:userid
+router.put('/:userId', async (req, res) => {
+    try {
+        const updateUser = await User.findOneAndUpdate({
+            _id: req.params.userId
+        }, {
+            $set: req.body,
+        }, {
+            runValidators: true,
+            new: true
+        });
+        if (!updateUser) {
+            return res.status(404).json({ message: "No user with this ID... Try again!" })
+        } res.status(200).json(updateUser)
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).json(err)
+    }
+})
+
+//DELETE: /api/users/:userId
+router.delete('/:userId', async (req, res) => {
+    try{
+        const removeUser = await User.findByIdAndDelete({
+            _id: req.params.userId
+        });
+        if(!removeUser){
+            return res.status(404).json({ message: "No user with this ID... Try again!"})
+        } 
+        await Thought.deleteMany({ _id: { $in: removeUser.thoughts }})
+        return res.status(200).json({ message: `Successfully deleted userId: ${req.params.userId} and its associated thoughts!`})
+    }
+    catch (err) {
+        console.log(err)
         res.status(500).json(err)
     }
 })
